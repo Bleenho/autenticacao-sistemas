37494855bca3d9e6ba456ce5c0b067aee5d9a7d1
Pablo RIbeiro <pableeenho@gmail.com>
2019-05-01 16:50:13 -0300

    Perfiç

--- src/main/java/br/com/facilita/autenticacao/api/controller/PerfilController.java
+++ src/main/java/br/com/facilita/autenticacao/api/controller/PerfilController.java
@@ -3,48 +3,32 @@ package br.com.facilita.autenticacao.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.facilita.autenticacao.api.request.ModuleRegisterRequest;
-import br.com.facilita.autenticacao.api.response.DefaultResponse;
-import br.com.facilita.autenticacao.exception.LoginException;
-import br.com.facilita.autenticacao.service.ModuleService;
+import br.com.facilita.autenticacao.api.request.PerfilRegisterRequest;
+import br.com.facilita.autenticacao.service.PerfilService;
 import io.swagger.annotations.Api;
 import javassist.NotFoundException;
 
 @RestController
-@RequestMapping("/modules")
-@Api(tags = "Modulo", value = "API's de modulos")
+@RequestMapping("/perfis")
+@Api(tags = "Perfil", value = "Um perfil possue acesso aos modulos de um sistema ou de varios sistemas")
 public class PerfilController {
 
-	private ModuleService service;
+	private PerfilService service;
 
 	@Autowired
-	public PerfilController(ModuleService service) {
+	public PerfilController(PerfilService service) {
 		this.service = service;
 	}
 
 	@PostMapping
-	public ResponseEntity<?> save(@RequestBody ModuleRegisterRequest moduleRegisterRequest) throws NotFoundException {
+	public ResponseEntity<?> save(@RequestBody PerfilRegisterRequest perfilRegisterRequest) throws NotFoundException {
 		return ResponseEntity.status(HttpStatus.CREATED)
-				.eTag(String.valueOf(service.createModule(moduleRegisterRequest))).build();
+				.eTag(String.valueOf(service.createPerfil(perfilRegisterRequest))).build();
 	}
 
-	@GetMapping
-	public ResponseEntity<DefaultResponse> list() throws LoginException {
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(DefaultResponse.builder().dado(service.allModule()).sucesso(true).build());
-	}
-
-	@DeleteMapping(value = "/{idModule}")
-	public ResponseEntity<?> delete(@PathVariable(name = "idModule") Long idModule) throws LoginException {
-		service.deleteModule(idModule);
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-	}
 }
--- src/main/java/br/com/facilita/autenticacao/service/PerfilService.java
+++ src/main/java/br/com/facilita/autenticacao/service/PerfilService.java
@@ -1,5 +1,9 @@
 package br.com.facilita.autenticacao.service;
 
+import br.com.facilita.autenticacao.api.request.PerfilRegisterRequest;
+
 public interface PerfilService {
+
+	Long createPerfil(PerfilRegisterRequest perfilRegisterRequest);
 
 }
