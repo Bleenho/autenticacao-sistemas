fca24a47d72a3ea54c563549c8e0199b1f2387cc
Pablo RIbeiro <pableeenho@gmail.com>
2019-05-05 12:41:09 -0300

    Redirect swagger page

--- src/main/java/br/com/facilita/autenticacao/Application.java
+++ src/main/java/br/com/facilita/autenticacao/Application.java
@@ -2,8 +2,11 @@ package br.com.facilita.autenticacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class Application {
 	/**
 	 * Main method
@@ -14,5 +17,10 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index(){
+		return "redirect:/swagger-ui.html";
 	}
 }
